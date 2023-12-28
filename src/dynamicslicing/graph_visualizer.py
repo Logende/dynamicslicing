00ecfd626f6e1dba6a4b30c578aeedd23c86a94f
Felix Neubauer <felix@neuby.de>
2023-12-28 21:01:43 +0100

    better visualization

--- src/dynamicslicing/graph_visualizer.py
+++ src/dynamicslicing/graph_visualizer.py
@@ -34,20 +34,23 @@ def save_rdf_graph(graph: rdflib.Graph, folder: Path, source: str):
 
     for s, p, o in graph.triples((None, None, None)):
         s_label = node_to_label(s, source_lines)
-        p_label = node_to_label(s, source_lines)
         o_label = node_to_label(o, source_lines)
-        nx_edges.append([s_label, o_label])
-        nx_edge_labels[s_label, o_label] = p_label
+        pair = [s_label, o_label]
+        if pair not in nx_edges:
+            nx_edges.append(pair)
+            nx_edge_labels[tuple(pair)] = str(p)
 
-        if p in (RELATIONSHIP_DEFINITION_HAS_DEPENDENT, RELATIONSHIP_INIT_IS_MANDATORY_FOR):
-            nx_edge_colors.append("brown")
-        elif p in (RELATIONSHIP_DEFINITION_IS_USED_BY, RELATIONSHIP_DEFINITION_IS_MODIFIED_BY):
-            nx_edge_colors.append("green")
+            if p in (RELATIONSHIP_DEFINITION_HAS_DEPENDENT, RELATIONSHIP_INIT_IS_MANDATORY_FOR):
+                nx_edge_colors.append("brown")
+            elif p in (RELATIONSHIP_DEFINITION_IS_USED_BY, RELATIONSHIP_DEFINITION_IS_MODIFIED_BY):
+                nx_edge_colors.append("green")
+            else:
+                raise RuntimeError("Unknown type of predicate: " + str(p))
 
     nx_graph = nx.DiGraph()
     nx_graph.add_edges_from(nx_edges)
-    pos = nx.spring_layout(nx_graph, scale=4, k=5/math.sqrt(nx_graph.order()))
-    plt.figure()
+    pos = nx.spring_layout(nx_graph, scale=2, k=5/math.sqrt(nx_graph.order()))
+    plt.figure(figsize=(10, 10))
     nx.draw(
         nx_graph, pos, edge_color=nx_edge_colors, width=1, linewidths=1,
         node_size=200, node_color='pink', alpha=0.6,
@@ -60,10 +63,9 @@ def save_rdf_graph(graph: rdflib.Graph, folder: Path, source: str):
             font_color='black',
             alpha=0.7
         )
-    plt.axis('off')
-    plt.show()
 
     plt.savefig(str(folder.joinpath("dependency_graph.png")))
+    plt.show()
 
     # Save it to turtle file also
     graph.serialize(destination=str(folder.joinpath("dependency_graph.ttl")), format='turtle')
